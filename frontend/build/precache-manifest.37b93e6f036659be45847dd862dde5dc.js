4e856cd9168c55de0effa8f62a695676e2f71152
halabe15 <halabe15@gmail.com>
2019-11-24 22:00:23 -0600

    - Backend deployed kubernetes

---
+++ frontend/build/precache-manifest.37b93e6f036659be45847dd862dde5dc.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "b785796210a502fb878b",
+    "url": "/static/css/main.4b619d62.chunk.css"
+  },
+  {
+    "revision": "b785796210a502fb878b",
+    "url": "/static/js/main.b7857962.chunk.js"
+  },
+  {
+    "revision": "9f694b0a815f18dfb367",
+    "url": "/static/js/1.9f694b0a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "91eeb1e1ef47adf250f7e256d7d39494",
+    "url": "/static/media/1.91eeb1e1.jpg"
+  },
+  {
+    "revision": "139504bed1b73dcd35c80505cd26f1f3",
+    "url": "/static/media/2.139504be.jpg"
+  },
+  {
+    "revision": "5eb0503a78234043453d4d3efb8b337c",
+    "url": "/static/media/3.5eb0503a.jpg"
+  },
+  {
+    "revision": "bf461484870bc4f2d251db9b83be51e3",
+    "url": "/static/media/2.bf461484.jpg"
+  },
+  {
+    "revision": "9048b69e3e7856f883d4b8f96c5dfa36",
+    "url": "/static/media/3.9048b69e.jpg"
+  },
+  {
+    "revision": "51d0aee814d290a3eef4a57edd69ce43",
+    "url": "/static/media/4.51d0aee8.jpg"
+  },
+  {
+    "revision": "5da2c5f2f441586d49da4dc934d811b2",
+    "url": "/static/media/1.5da2c5f2.jpg"
+  },
+  {
+    "revision": "8a902fe12a1634926cd622371316d096",
+    "url": "/static/media/2.8a902fe1.jpg"
+  },
+  {
+    "revision": "4f28ed7da320b9f2421d2c146f24791a",
+    "url": "/static/media/3.4f28ed7d.jpg"
+  },
+  {
+    "revision": "4bfbada2cbc17d118866bc19c3957356",
+    "url": "/static/media/4.4bfbada2.jpg"
+  },
+  {
+    "revision": "539ea756d2bdd2b8b603a1dea10cbfc9",
+    "url": "/static/media/BotonDulce.539ea756.jpeg"
+  },
+  {
+    "revision": "b2096141aaac35139ac5bbc588127e15",
+    "url": "/static/media/BotonSalada.b2096141.jpeg"
+  },
+  {
+    "revision": "e09ca8c32925c1fd925a50862a443fe9",
+    "url": "/static/media/TengoPecera.e09ca8c3.jpeg"
+  },
+  {
+    "revision": "c90eadf4af97ddfcf0f6e3189d14fdd0",
+    "url": "/static/media/QuieroPecera.c90eadf4.jpeg"
+  },
+  {
+    "revision": "1bba514da292205932c671d1c77757be",
+    "url": "/static/media/Paquete.1bba514d.jpeg"
+  },
+  {
+    "revision": "d4e19d7462947addf366040b9b0163f0",
+    "url": "/static/media/Configurar.d4e19d74.jpeg"
+  },
+  {
+    "revision": "3003836a778ebeccc9af3f91003c1941",
+    "url": "/static/media/1.3003836a.jpg"
+  },
+  {
+    "revision": "0ffbd73ab7d0bbe50341221137e40f62",
+    "url": "/static/media/3.0ffbd73a.jpg"
+  },
+  {
+    "revision": "3f651d75f1bb65e75806501a536ea260",
+    "url": "/static/media/2.3f651d75.jpg"
+  },
+  {
+    "revision": "457189b6e0b430dc39c5942682593296",
+    "url": "/static/media/4.457189b6.jpg"
+  },
+  {
+    "revision": "a642e8048bdb29a234067a6fcb81ecb7",
+    "url": "/static/media/5.a642e804.jpg"
+  },
+  {
+    "revision": "f61788dd608a604e5e56c1396b0be51c",
+    "url": "/static/media/6.f61788dd.jpg"
+  },
+  {
+    "revision": "e875eb873aecf3e61d5c79dcd24d9bb7",
+    "url": "/static/media/7.e875eb87.jpg"
+  },
+  {
+    "revision": "4f08c8850ef19587d549c4dc74928631",
+    "url": "/index.html"
+  }
+];
