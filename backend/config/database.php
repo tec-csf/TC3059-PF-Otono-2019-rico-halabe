ad773ca993258a7696694a90c42699d10d8ecdf7
halabe15 <halabe15@gmail.com>
2019-11-25 05:45:46 -0600

    - el mejor intento...

--- backend/config/database.php
+++ backend/config/database.php
@@ -42,12 +42,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '35.224.22.20'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'database' => env('DB_DATABASE', 'mipecera'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', 'PCBgNJoCBJu0sy19'),
+            'unix_socket' => env('DB_SOCKET', '/cloudsql/final-project-cloud-apps:us-central1:mipecera'),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
